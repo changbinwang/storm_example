b574071742203d2d1e9eae9d1fc3a4225620ab61
Owen Wang <owen2785@gmail.com>
2012-03-02 13:25:00 +0800

    import storm example

---
+++ project.clj
@@ -0,0 +1,20 @@
+(defproject storm-starter "0.0.1-SNAPSHOT"
+  :source-path "src/clj"
+  :java-source-path "src/jvm"
+  :javac-options {:debug "true" :fork "true"}
+  :resources-path "multilang"
+  :aot :all
+  :jvm-opts ["-Djava.library.path=/usr/local/lib:/opt/local/lib:/usr/lib"]
+  :repositories {
+                 "twitter4j" "http://twitter4j.org/maven2"
+                 }
+
+  :dependencies [[org.clojure/clojure "1.2.0"]
+                 [org.clojure/clojure-contrib "1.2.0"]
+                 [org.twitter4j/twitter4j-core "2.2.5-SNAPSHOT"]
+                 [org.twitter4j/twitter4j-stream "2.2.5-SNAPSHOT"]
+                 ]
+
+  :dev-dependencies [[storm "0.7.0"]
+                     ])
+
